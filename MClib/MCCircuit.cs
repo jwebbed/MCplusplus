2324083e5ac54f8fedf28ea2326ee676752214c9
Jonathan Webb <jwebbed@bell.net>
2014-05-09 00:06:46 -0400

    shtuff

---
+++ MClib/MCCircuit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MClib
+{
+    public class MCCircuit : IMCObject
+    {
+        private List<MCWire> _inputs;
+        private List<MCWire> _outputs;
+
+        public List<MCWire> inputs { get { return this._inputs; } }
+        public List<MCWire> outputs { get { return this._outputs; } }
+        public MCCircuit(List<MCWire> inputs, List<MCWire> outputs)
+        {
+            this._inputs = inputs;
+            this._outputs = outputs;
+        }
+
+        #region NYI
+
+        void generateCircuit()
+        {
+            throw new NotImplementedException();
+        }
+        void optimize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MCCircuit connect(IMCObject a, IMCObject b)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ MClib/MCObject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MClib
+{
+    public enum WireDirection { input, output }
+    public class MCWire
+    {
+        IMCObject _base_object;
+        MCWire connected_wire;
+        WireDirection dir;
+        
+        public MCWire(IMCObject obj, WireDirection dir)
+        {
+            this._base_object = obj;
+            this.connected_wire = null;
+            this.dir = dir;
+        }
+
+        bool isConnected()
+        {
+            return this.connected_wire != null;
+        }
+        void connectTo(MCWire wire)
+        {
+            this.connected_wire = wire;
+        }
+    }
+
+    public interface IMCObject
+    {
+        List<MCWire> inputs { get; }
+        List<MCWire> outputs { get; }
+        MCCircuit connect(IMCObject a, IMCObject b);
+    }
+
+    public interface IMCGate : IMCObject
+    {
+
+    }
+}
