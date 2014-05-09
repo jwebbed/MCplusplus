f25e63a0f90db818f70cac4c6e018aab23435545
Jonathan Webb <jwebbed@bell.net>
2014-05-09 02:02:02 -0400

    I think I added some spaces

---
+++ MCppParser/MCPPGrammar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Irony.Parsing;
+
+namespace MCPPGrammar
+{
+    public class MCPPGrammar : Irony.Parsing.Grammar
+    {
+        public MCPPGrammar()
+        {
+            this.MarkReservedWords("module", "input", "output", "assign");
+        }
+    }
+}
